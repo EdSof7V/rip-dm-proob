9e835dcfa89088ce2300ded53ce32e0e37f542a4
Edgar Almoguera <edgar.almoguera@analitika.cloud>
2025-03-04 03:15:32 -0500

    push app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        AZURE_AD_CLIENT_ID:"bea332aa-43c3-40cb-8055-c7bf6f20a994",
+        AZURE_AD_CLIENT_SECRET:"5l~8Q~LUo-_x9J1mDlkawPDafzPKt_nIGAf.CcRC",
+        AZURE_AD_TENANT_ID:"4c04f3e1-4882-493e-b3bb-81f8bee5b7c0",
+
+        NEXT_PUBLIC_API_URL:"https://my-app-430707315045.southamerica-west1.run.app",
+        NEXTAUTH_SECRET:"d35e00b42eb505d1b7ee257b9d2cce38",
+    }
+};
 
 export default nextConfig;
